92b6e8eeb8cce4e81354a0d47158c180efaa5662
Nick Sutterer <apotonick@gmail.com>
2011-01-26 16:05:47 +0100

    added fake_server for tests. stolen from Restfulie.

---
+++ test/fake_server.rb
@@ -0,0 +1,27 @@
+require 'rubygems'
+require 'sinatra/base'
+require 'active_support'
+require 'json'
+
+class FakeServer < Sinatra::Base
+  put "/test/put" do
+    request.body
+  end
+
+  get "/test/get" do
+    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n  <test-object>\n<id>9</id>\n  </test-object>\n"
+  end
+
+  get "/test/:id" do
+    "<test><id>#{params[:id]}</id></test>"
+  end
+
+  
+
+
+  get "/eine-ressource-in-xml" do
+    "<name>eine-ressource</name>"
+  end
+end
+
+FakeServer.run! :host => 'localhost', :port => 9999
