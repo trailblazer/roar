8ab119ee672c2d7ced6d08c023a951300d45c7a7
Nick Sutterer <apotonick@gmail.com>
2011-02-01 22:54:35 +0100

    added a first integration test to show how that stuff is supposed to work. how cool is that?

--- test/fake_server.rb
+++ test/fake_server.rb
@@ -16,12 +16,41 @@ class FakeServer < Sinatra::Base
     "<test><id>#{params[:id]}</id></test>"
   end
 
-  
+  get "/article/:id" do
+    "<article><id>#{params[:id]}</id></article>"
+  end
 
 
   get "/eine-ressource-in-xml" do
     "<name>eine-ressource</name>"
   end
+  
+  get "/orders/2" do
+"<order>
+  <id>#{params[:id]}</id>
+  <item>
+    <number>1</number>
+    <article>
+      <uri>http://localhost:9999/article/666</uri>
+    </article>
+  </item>
+  <item>
+    <number>2</number>
+    <article>
+      <uri>http://localhost:9999/article/22</uri>
+    </article>
+  </item>
+</order>
+"    
+  end
+  
+  get "/orders/:id" do
+"<order>
+  <id>#{params[:id]}</id>
+</order>
+"    
+  end
+  
 end
 
 FakeServer.run! :host => 'localhost', :port => 9999
---
+++ test/integration/order_client_test.rb
@@ -0,0 +1,76 @@
+require 'test_helper'
+
+class Article
+  include Roar::Client::ModelMethods
+  include Roar::Representer::Xml
+end
+
+
+class Position
+  include Roar::Client::ModelMethods
+  include Roar::Representer::Xml
+  
+  xml do
+    has_proxied :article, :class => Article
+  end
+  
+  def number
+    attributes["number"]
+  end
+  
+  def article
+    attributes["article"]
+  end
+end
+
+class Order
+  include Roar::Client::ModelMethods
+  include Roar::Model::HttpVerbs
+  
+  # FIXME: how to know xml?
+include Roar::Representer::Xml
+  
+  def self.resource_host; "http://localhost:9999/orders/" end
+  
+  xml do
+    has_many :items, :class => Position
+  end
+  
+  def items
+    attributes["items"]
+  end
+end
+
+
+
+class OrderClientIntegrationTest < MiniTest::Spec
+  describe "A classic shopping cart scenario" do
+    describe "getting an empty order" do
+      it "retrieves the representation" do
+        @order = Order.get("1")
+        assert_kind_of Order, @order
+        assert_equal [], @order.items
+      end
+      
+    end
+    
+    describe "order with items" do
+      it "retrieves the representation" do
+        @order = Order.get("2")
+        assert_kind_of Order, @order
+        
+        @items = @order.items
+        assert_equal "1", @items[0].number
+        assert_equal "2", @items[1].number
+        
+        # a grouping describe "proxy" would be cool here.
+        @article = @items[0].article
+        @article.finalize!
+        assert_equal({"id" => "666"}, @article.attributes)
+        
+      end
+      
+    end
+    
+  end
+end
