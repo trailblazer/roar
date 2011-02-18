217f8837bf60f90ca967503485c721b0a285c189
Nick Sutterer <apotonick@gmail.com>
2011-02-19 00:43:06 +0100

    Roxml.has_one now really queries the represented class.

--- lib/roar/representer/roxml.rb
+++ lib/roar/representer/roxml.rb
@@ -30,34 +30,31 @@ module Roar
       
       class << self
         def from_xml(data, *initialization_args)    # overwritten from roxml.
-        xml = ROXML::XML::Node.from(data)
-        
-        config = roxml_attrs.collect { |attr| attr.to_ref(nil, self) }  # FIXME: see Definition#initialize
-        
-        represented = represented_class.new(*initialization_args) # DISCUSS: *args useful?
-        
-        
-        config.each do |ref|
-          value = ref.value_in(xml)
-          represented.respond_to?(ref.opts.setter) \
-              ? represented.send(ref.opts.setter, value) \
-              : represented.instance_variable_set(ref.opts.instance_variable_name, value)
+          xml = ROXML::XML::Node.from(data)
+          
+          config = roxml_attrs.collect { |attr| attr.to_ref(nil, self) }  # FIXME: see Definition#initialize
+          
+          represented = represented_class.new(*initialization_args) # DISCUSS: *args useful?
+          
+          
+          config.each do |ref|
+            value = ref.value_in(xml)
+            represented.respond_to?(ref.opts.setter) \
+                ? represented.send(ref.opts.setter, value) \
+                : represented.instance_variable_set(ref.opts.instance_variable_name, value)
+          end
+          
+          represented
         end
         
-        represented
-      end
-      
-      
-      
         def has_one(attr_name, options={})
-          if options[:class] 
-          options.delete(:class)
-            options[:as] = RoxmlRepresenterFunctionalTest::ItemApplicationXml#options.delete(:class) 
+          if klass = options.delete(:class)
+            options[:as] = klass.representer_class_for(mime_type) or raise "No representer found for #{mime_type}"
+            puts options.inspect
           end
           
           xml_accessor attr_name, options
-        end
-        
+        end     
       end
       
     end
--- test/roxml_representer_test.rb
+++ test/roxml_representer_test.rb
@@ -7,6 +7,11 @@ require "roxml"
 require "roar/representer/roxml"
 
 class RoxmlRepresenterFunctionalTest < MiniTest::Spec
+  class ModelWithItem < TestModel # TODO: use me!
+    def item; attributes["item"]; end
+    def item=(item); attributes["item"]=item; end
+  end
+  
   
   class Item < TestModel
     def value=(value)
@@ -50,10 +55,10 @@ class RoxmlRepresenterFunctionalTest < MiniTest::Spec
     end
     
     
-    describe "has_one" do
+    describe "with a typed attribute" do
       before do
         @c = Class.new(TestXmlRepresenter) do
-          has_one :item, :class => Item
+          xml_accessor :item, :as => ItemApplicationXml # note that we pass the representer here!
         end
         
         @o.instance_eval do
@@ -76,7 +81,38 @@ class RoxmlRepresenterFunctionalTest < MiniTest::Spec
         m = @r.class.from_xml("<test><id>1</id><item><value>beer</value></item>\n</test>")
         assert_equal(TestModel.new("id" => "1", "item" => Item.new("value" => "beer")), m)
       end
+    end
+  
+  end
+end
+
+
+require "roar/model/representable"
+class RoxmlRepresenterUnitTest
+  class Item
+    include Roar::Model::Representable
+    
+    represents "application/xml", :with => Hash
+  end
+  
+  describe "has_one" do
+    before do
+      @c = Class.new(Roar::Representer::Roxml) do
+        self.mime_type= "application/xml"
+        has_one :item, :class => Item
+      end
+    end
+    
+    it "saves the representer class" do
+      assert_equal Hash, @c.roxml_attrs.first.sought_type
+    end
+    
+    it "raises an exception if no representer class is found" do
+      @c.mime_type = "text/html"
       
+      assert_raises RuntimeError do
+        @c.has_one :id, :class => Item  # asking Item for "text/html" representer fails.
+      end
     end
   end
 end
