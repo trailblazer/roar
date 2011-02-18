33a2dbb6b33754ea63d9b8140c88434d36a7aa4f
Nick Sutterer <apotonick@gmail.com>
2011-02-18 14:08:38 +0100

    an ugly prototype version of Representer::Roxml.

--- test/roxml_representer_test.rb
+++ test/roxml_representer_test.rb
@@ -10,22 +10,67 @@ module Roar
       include ROXML
       
       def serialize(attributes)
-        attributes.each do |k,v|  # TODO: use roxml?
-          send("#{k}=", v) and next unless k=="item"
-          self.item= ItemApplicationXml.new(v)
-        end
-        
         to_xml.serialize
       end
       
+      def to_xml(*)
+        represented.attributes.each do |k,v|  # FIXME: replace with xml_attributes from roxml.
+          send("#{k}=", v)
+        end
+        
+        # FIXME: generic model->representer.
+        self.class.roxml_attrs.each { |attr| 
+          #puts attr.sought_type.inspect
+        
+          if attr.name == "item"
+            v = represented.send(:item)
+            
+            self.item= RoxmlRepresenterFunctionalTest::ItemApplicationXml.new(v) if v
+          end
+        }
+        
+        super
+      end
+      
+      
+      
+      
+      
       def deserialize(body)
-
+        representer = self.class.from_xml(body, "application/xml")
+        
+        attributes = {}
+        representer.roxml_references.collect do |a|
+          attributes[a.name] = representer.send(a.name)
+        end
+        attributes
       end
       
       class << self
+        def from_xml(data, *initialization_args)    # overwritten from roxml.
+        xml = ROXML::XML::Node.from(data)
+        
+        config = roxml_attrs.collect { |attr| attr.to_ref(nil, self) }  # FIXME: see Definition#initialize
+        
+        represented = represented_class.new(*initialization_args) # DISCUSS: *args useful?
+        
+        
+        config.each do |ref|
+          value = ref.value_in(xml)
+          represented.respond_to?(ref.opts.setter) \
+              ? represented.send(ref.opts.setter, value) \
+              : represented.instance_variable_set(ref.opts.instance_variable_name, value)
+        end
+        
+        represented
+      end
+      
+      
+      
         def has_one(attr_name, options={})
           if options[:class] 
-            options[:as] = options.delete(:class) 
+          options.delete(:class)
+            options[:as] = RoxmlRepresenterFunctionalTest::ItemApplicationXml#options.delete(:class) 
             #options[:to_xml] = Proc.new{|instance| instance and instance.to("application/xml")}
           end
           
@@ -38,73 +83,51 @@ module Roar
   end
 end
 
-class ItemApplicationXml < Roar::Representer::Roxml
-  def initialize(represented)
-        @represented = represented
-      end
-      def to_xml(*args)
-        @represented.to_xml(*args)
-      end
-      
-end
 
-class ItemRoxmlRepresenterFunctionalTest < MiniTest::Spec
-  it "subject" do
-    assert_equal "expected", ItemApplicationXml.new()
-  end
-  
-end 
 
 class RoxmlRepresenterFunctionalTest < MiniTest::Spec
   
-  class Item
-    include ROXML
+  class Item < TestModel
+    def value=(value)
+      @attributes[:value] = value
+    end
+  end
+  
+  class ItemApplicationXml < Roar::Representer::Roxml
+    self.represented_class = Item
     
     xml_accessor :value
-    
-    #attr_reader :value
-    
-    def initialize(value)
-      @value = value
-    end
-    
-    def to(mime_type)
-      raise unless mime_type == "application/xml"
-      self  # ROXML expects self!
-    end
-    
-    def self.from(mime_type, string)
-      raise unless mime_type == "ruby/serialized"
-      new Marshal.load(string)
-    end
-    
-    
-    
-    
-    def ==(b)
-      value == b.value
-    end
   end
   
   describe "RoxmlRepresenter" do
     before do
-      @r = Class.new(Roar::Representer::Roxml).new("application/xml")
+      @m = {"id" => "1"}
+      @o = TestModel.new(@m)
+      
+      @r = Class.new(Roar::Representer::Roxml).new(@o)
       @r.class.instance_eval do
+        puts "setting in #{self}"
+        
+        
         xml_name :test  # FIXME: move to subclass.
         has_one :id
       end
-      
-      @m = {:id => 1}
+      @r.class.represented_class= TestModel
     end
     
     describe "without options" do
       it "#serialize returns the serialized model" do
-        assert_match_xml "<test><id>1</id></test>", @r.serialize(@m)
+        assert_exactly_match_xml "<test><id>1</id></test>", @r.serialize(@m)
       end
       
-      it "#deserialize returns the attributes" do
-        assert_equal @m, @r.deserialize("\x04\b{\x06:\aidi\x06")
+      it ".from_xml returns the deserialized model" do
+        assert_equal TestModel.new("id" => "1"), @r.class.from_xml("<test><id>1</id></test>")
       end
+      
+      it "#to_xml returns the serialized xml" do
+        assert_exactly_match_xml "<test><id>1</id></test>", @r.serialize(@m)
+      end
+      
     end
     
     
@@ -113,20 +136,27 @@ class RoxmlRepresenterFunctionalTest < MiniTest::Spec
         @r.class.instance_eval do
           has_one :item, :class => Item
         end
+        
+        
+        @o.instance_eval do
+          def item; attributes[:item]; end
+          def item=(item); attributes[:item]=item; end
+        end
+        
       end
       
-      it "#serialize skips empty :item" do
-        assert_match_xml "<test><id>1</id></test>", @r.serialize(@m)
+      it "#to_xml skips empty :item" do
+        assert_exactly_match_xml "<test><id>1</id></test>", @r.to_xml.serialize
       end
       
-      it "#serialize delegates to item#to" do
-        @m = {:id => 1, "item" => Item.new("beer")}
-        assert_equal "<test>\n  <id>1</id>\n  <item>\n    <value>beer</value>\n  </item>\n</test>", @r.serialize(@m)
+      it "#to_xml delegates to ItemXmlRepresenter#to_xml" do
+        @o.item = Item.new(:value => "Bier")
+        assert_exactly_match_xml "<test><id>1</id><item><value>Bier</value></item>\n</test>", @r.to_xml.serialize
       end
       
-      it "#deserialize typecasts :item" do
-        m = @r.deserialize("\x04\b{\a:\aidi\x06I\"\titem\x06:\x06EF\"\x13\x04\bI\"\tbeer\x06:\x06EF")
-        assert_equal({:id => 1, "item" => Item.new("beer")}, m)
+      it ".from_xml typecasts :item" do
+        m = @r.class.from_xml("<test><id>1</id><item><value>beer</value></item>\n</test>")
+        assert_equal(TestModel.new("id" => "1", "item" => Item.new(:value => "beer")), m)
       end
       
     end
