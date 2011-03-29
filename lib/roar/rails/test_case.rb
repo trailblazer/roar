c47f89bcebf28383dd11595fc2cecbdd1d145e46
Nick Sutterer <apotonick@gmail.com>
2011-03-29 11:18:34 +0200

    added TestCase#assert_body and #assert_headers which can be invoked in #assert_response.

--- lib/roar/rails/test_case.rb
+++ lib/roar/rails/test_case.rb
@@ -18,10 +18,23 @@ ActionController::TestCase.class_eval do
   
   def assert_response(status, headers={})  # FIXME: allow message.
     super
+    
+    if headers.is_a?(Hash)
+      assert_headers(headers)
+    else
+      assert_body(headers)
+    end
+  end
+  
+  def assert_headers(headers)
     headers.each_pair do |k,v|
       assert_equal v, @response.headers[k]
     end
-    
   end
+  
+  def assert_body(body)
+    assert_equal body, @response.body
+  end
+  
   
 end
