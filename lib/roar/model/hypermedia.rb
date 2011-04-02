a354833532aa8e5ac7682802be72b407eeeacd45
Nick Sutterer <apotonick@gmail.com>
2011-04-02 23:49:52 +0200

    adding an UNTESTED module Hypermedia for models to push hypermedia links into models which are then queried when representing.

---
+++ lib/roar/model/hypermedia.rb
@@ -0,0 +1,19 @@
+require "roar/model"
+
+module Roar
+  module Model
+    # Adds links methods to the model which can then be used for hypermedia links when
+    # representing the model.
+    module Hypermedia # TODO: test me.
+      def links=(links)
+        @links = links.collect do |link|
+          Roar::Representer::Roxml::Hyperlink.for_attributes(link)
+        end
+      end
+      
+      def links
+        @links
+      end
+    end
+  end
+end
