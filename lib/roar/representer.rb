2fdb0ce10809ce8c76e3095bd3d83146382bee35
Nick Sutterer <apotonick@gmail.com>
2011-04-23 21:23:43 +0200

    removing old shit.

--- lib/roar/representer.rb
+++ lib/roar/representer.rb
@@ -3,34 +3,6 @@ module Roar
   # serialize
   module Representer
     class Base
-      class_attribute :represented_class
-      class_attribute :mime_type
-      
-      class << self
-        def represents(klass, options) # DISCUSS: what if we don't want to hard-wire these settings into the representer?
-          self.represented_class = klass
-          self.mime_type = options[:as]
-        end
-        
-      private
-        def from_xml(*)
-        end
-      end
-      
-      # DISCUSS: serialize on instance?
-      def serialize(represented, mime_type)
-        
-      end
-      
-      #def serialize_model(model)
-      #  serialize(model.attributes)
-      #end
-      
-      
-    private
-      def to_xml(*) # call #serialize instead.
-      end
-      
     end
     
     require "roxml/definition"
