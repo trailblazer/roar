c030c1808a1eb5c49ffec4640ceecf3e06a49ee2
Nick Sutterer <apotonick@gmail.com>
2011-05-25 10:12:44 +0200

    HttpVerbs methods now requires the media format for now.

---
+++ test/http_verbs_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+require 'roar/representer/feature/http_verbs'
+
+class HttpVerbsTest < MiniTest::Spec
+  class BandRepresenter < Roar::Representer::XML
+    self.representation_name = :band
+    
+    property :name
+    property :label
+    
+    include Roar::Representer::Feature::HttpVerbs
+  end
+  
+  describe "HttpVerbs" do
+    before do
+      @r = BandRepresenter.new
+    end
+    
+    # TODO: assert that Restfulie#post receives the correct document.
+    
+    it "#post deserializes the incoming representation and returns it" do
+      @r.name = "Strung Out"
+      rep = @r.post("http://localhost:9999/band", "application/xml")
+      assert_equal "Strung Out", rep.name
+      assert_equal "n/a", rep.label
+    end
+    
+    it "#put deserializes the incoming representation and returns it" do
+      @r.name   = "Strung Out"
+      @r.label  = "Fat Wreck"
+      rep = @r.put("http://localhost:9999/band/strungout", "application/xml")
+      assert_equal "Strung Out", rep.name
+      assert_equal "Fat Wreck", rep.label
+    end
+  end
+end
