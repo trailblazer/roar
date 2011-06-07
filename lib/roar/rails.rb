101618b291368f8dcd928b8ec388a031eea17e96
Nick Sutterer <apotonick@gmail.com>
2011-06-07 14:33:42 +0200

    require json representer for rails per default.

--- lib/roar/rails.rb
+++ lib/roar/rails.rb
@@ -1,6 +1,7 @@
 require 'roar/rails/controller_methods'
 require 'roar/representer'
 require 'roar/representer/xml'  # TODO: make dynamically.
+require 'roar/representer/json' # TODO: make dynamically.
 require 'roar/rails/representer_methods'
 require 'roar/representer/feature/model_representing'
 
