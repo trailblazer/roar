e7b556f0eb2f5ab7740d0d8be16722d3bbba5e1f
Nick Sutterer <apotonick@gmail.com>
2011-10-29 14:41:16 +0200

    restructured hypermedia tests and added edge cases.

--- test/xml_hypermedia_test.rb
+++ test/xml_hypermedia_test.rb
@@ -6,43 +6,56 @@ class XmlHypermediaTest < MiniTest::Spec
       @c = Class.new do
         include Roar::Representer::XML
         self.representation_name= :wuff
-        representable_property :id
+        
+        property :id
+        
         link :self do "http://self" end
         link :next do "http://next/#{id}" end
       end
-      @r = @c.new
     end
     
-    it "responds to #links" do
-      assert_equal nil, @r.links
-    end
-    
-    it "computes links in #from_attributes" do
-      @r = @c.from_attributes({"id" => 1})
-      assert_equal 2, @r.links.size
-      assert_equal({"rel"=>:self, "href"=>"http://self"}, @r.links.first.to_attributes)
-      assert_equal({"rel"=>:next, "href"=>"http://next/1"}, @r.links.last.to_attributes) 
-    end
-    
-    it "extracts links from XML" do
-      @r = @c.deserialize(%{
-      <order>
-        <link rel="self" href="http://self">
-      </order>
-      })
+    describe "#deserialize" do
+      it "extracts links from XML" do
+        @r = @c.deserialize(%{
+        <order>
+          <link rel="self" href="http://self">
+        </order>
+        })
+        
+        assert_kind_of Roar::Representer::Feature::Hypermedia::LinkCollection, @r.links
+        assert_equal 1, @r.links.size
+        assert_equal({"rel"=>"self", "href"=>"http://self"}, @r.links.first.to_attributes) 
+      end
       
-      assert_kind_of Roar::Representer::Feature::Hypermedia::LinkCollection, @r.links
-      assert_equal 1, @r.links.size
-      assert_equal({"rel"=>"self", "href"=>"http://self"}, @r.links.first.to_attributes) 
+      it "works even if there are no links in the document" do
+        @r = @c.deserialize(%{<order/>})
+        assert_equal [], @r.links
+      end
     end
     
-    it "renders <link> correctly in XML" do
-      assert_xml_equal %{<wuff>
-  <id>1</id>
-  <link rel="self" href="http://self"/>
-  <link rel="next" href="http://next/1"/>
-</wuff><expected />}, @c.from_attributes({"id" => 1}).serialize
-    end
-    
+    # FIXME: MiniTest::Spec swallows this block:
+    #describe "#serialize" do
+      it "renders <link> correctly in XML" do
+        assert_xml_equal %{
+        <wuff>
+          <id>1</id>
+          <link rel="self" href="http://self"/>
+          <link rel="next" href="http://next/1"/>
+        </wuff>}, @c.from_attributes({"id" => 1}).serialize
+      end
+      
+      it "skips link when there are no" do
+        rep = Class.new do
+          include Roar::Representer::XML
+          self.representation_name= :wuff
+          property :id
+        end.from_attributes(:id => 1)
+        
+        assert_xml_equal %{
+        <wuff>
+          <id>1</id>
+        </wuff>}, rep.serialize
+      end
+    #end
   end
 end
