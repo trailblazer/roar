00ce30af97f60066f881bf5df8d784854c91109d
Nick Sutterer <apotonick@gmail.com>
2011-10-29 11:25:27 +0200

    no need for monkey-patching Representable::Definition anymore.

--- lib/representable_extensions.rb
+++
@@ -1,6 +0,0 @@
-Representable::Definition.class_eval do
-  # Populate the representer's attribute with the right value.
-  def populate(representer, attributes)
-    representer.public_send("#{accessor}=", attributes[accessor])
-  end
-end
--- lib/roar/representer/base.rb
+++ lib/roar/representer/base.rb
@@ -1,5 +1,4 @@
 require 'representable'
-require 'representable_extensions'
 
 module Roar
   module Representer
