b1539bf3c6d469b42b01ffafb0d7a43cb11a503c
Nick Sutterer <apotonick@gmail.com>
2011-10-30 14:47:55 +0100

    removed xml_hypermedia_test since it's in hypermedia_feature_test already.

--- test/hypermedia_feature_test.rb
+++ test/hypermedia_feature_test.rb
@@ -3,28 +3,30 @@ require 'roar/representer/feature/hypermedia'
 
 class HypermediaTest
   describe "Hypermedia Feature" do
-    class Bookmarks
-      include Roar::Representer::XML
-      include Roar::Representer::Feature::Hypermedia
-    end
-    
     before do
-      @bookmarks = Class.new(Bookmarks) do
-        self.representation_name = :bookmarks
+      @bookmarks = Class.new do
+        include Roar::Representer::XML
+        include Roar::Representer::Feature::Hypermedia
+        
+        self.representation_name = "bookmarks"
+      end
+      
+      @bookmarks_with_links = Class.new(@bookmarks)
+      @bookmarks_with_links.class_eval do
+        property :id
+        link :self do "http://bookmarks" end
+        link :all do "http://bookmarks/all" end
       end
     end
     
+    
     describe "#serialize" do
-      it "sets up links before rendering" do
-        @bookmarks.class_eval do
-          link :self do "http://bookmarks" end
-          link :all do "http://bookmarks/all" end
-        end
-        
+      it "sets up links and renders <link> correctly in XML" do
         assert_xml_equal '<bookmarks>
+                            <id>1</id>
                             <link rel="self" href="http://bookmarks"/>
                             <link rel="all" href="http://bookmarks/all"/>
-                          </bookmarks>', @bookmarks.new.serialize
+                          </bookmarks>', @bookmarks_with_links.from_attributes(:id => 1).serialize
       end
       
       it "still works even if there are no links defined" do
@@ -33,9 +35,28 @@ class HypermediaTest
     end
     
     
+    describe "#deserialize" do
+      it "extracts links from document" do
+        doc = @bookmarks_with_links.deserialize(%{
+        <bookmarks>
+          <link rel="self" href="http://bookmarks">
+        </bookmarks>
+        })
+        
+        assert_kind_of Roar::Representer::Feature::Hypermedia::LinkCollection, doc.links
+        assert_equal 1, doc.links.size
+        assert_equal({"rel"=>"self", "href"=>"http://bookmarks"}, doc.links.first.to_attributes) 
+      end
+      
+      it "sets up an empty link list if no links found in the document" do
+        assert_equal [], @bookmarks_with_links.deserialize(%{<bookmarks/>}).links
+      end
+    end
+    
+    
     describe "#links" do
       before do
-        @set  = Bookmarks.new
+        @set  = @bookmarks.new
         hyper = Roar::Representer::XML::Hyperlink
         
         @set.links = [hyper.from_attributes({"rel" => "self", "href" => "http://self"}),
@@ -71,7 +92,7 @@ class HypermediaTest
           link :self
         end
         
-        assert_equal "links", @bookmarks.find_links_definition.name
+        assert_equal "link", @bookmarks.find_links_definition.name
       end
       
       it "returns nil if no links defined" do
