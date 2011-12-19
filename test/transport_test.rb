cb05b35848e4b17b5a8be614e68512bdcbf91c17
Nick Sutterer <apotonick@gmail.com>
2011-12-19 14:20:34 +0100

    Transport now contains class methods, only. should be considered a private module and not a feature.

--- test/transport_test.rb
+++ test/transport_test.rb
@@ -4,26 +4,23 @@ require 'roar/representer/feature/transport'
 class TransportTest < MiniTest::Spec
   describe "Transport" do
     before do
-      @klass = Class.new(Object) do
-        include Roar::Representer::Feature::Transport
-      end
-      @o = @klass.new
+      @transport = Roar::Representer::Feature::Transport
     end
     
     it "#get_uri returns response" do
-      assert_equal "<method>get</method>",  @o.get_uri("http://localhost:9999/method", "application/xml").body
+      assert_equal "<method>get</method>",  @transport.get_uri("http://localhost:9999/method", "application/xml").body
     end
     
     it "#post_uri returns response" do
-      assert_equal "<method>post</method>",  @o.post_uri("http://localhost:9999/method", "booty", "application/xml").body
+      assert_equal "<method>post</method>",  @transport.post_uri("http://localhost:9999/method", "booty", "application/xml").body
     end
     
     it "#put_uri returns response" do
-      assert_equal "<method>put</method>",  @o.put_uri("http://localhost:9999/method", "booty", "application/xml").body
+      assert_equal "<method>put</method>",  @transport.put_uri("http://localhost:9999/method", "booty", "application/xml").body
     end
     
     it "#delete_uri returns response" do
-      assert_equal "<method>delete</method>",  @o.delete_uri("http://localhost:9999/method", "application/xml").body
+      assert_equal "<method>delete</method>",  @transport.delete_uri("http://localhost:9999/method", "application/xml").body
     end
     
     # TODO: how to get PATCH into Sinatra?
