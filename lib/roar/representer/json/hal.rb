f2c69776172f8682d5963ad43a3b8958edcffdb3
Nick Sutterer <apotonick@gmail.com>
2012-03-15 17:00:43 +0100

    moved _links behaviour to JSON::HAL::Links so it can be included without the _embedded behaviour.

--- lib/roar/representer/json/hal.rb
+++ lib/roar/representer/json/hal.rb
@@ -5,36 +5,57 @@ module Roar::Representer
         base.class_eval do
           include Roar::Representer::JSON
           include Roar::Representer::Feature::Hypermedia
-          extend ClassMethods
+          extend Links::ClassMethods
         end
       end
       
-      
-      module LinkCollectionRepresenter
-        include JSON
+      # Including this module in your representer will render and parse your embedded hyperlinks
+      # following the HAL specification: http://stateless.co/hal_specification.html
+      #
+      #   module SongRepresenter
+      #     include Roar::Representer::JSON
+      #     include Roar::Representer::JSON::HAL::Links
+      #     
+      #     link :self { "http://self" }
+      #   end
+      #
+      # Renders to
+      #
+      #   {"_links":{"self":"http://self"}}
+      module Links
+        def self.included(base)
+          base.class_eval do
+            include Roar::Representer::Feature::Hypermedia
+            extend Links::ClassMethods
+          end
+        end
         
-        def to_hash(*)
-          {}.tap do |hash|
-            each do |link|
-              hash[link.rel] = link.href
+        module LinkCollectionRepresenter
+          include JSON
+          
+          def to_hash(*)
+            {}.tap do |hash|
+              each do |link|
+                hash[link.rel] = link.href
+              end
             end
           end
+          
+          def from_hash(json, *)
+            json.each do |k, v|
+              self << Feature::Hypermedia::Hyperlink.new(:rel => k, :href => v)
+            end
+            self
+          end
         end
         
-        def from_hash(json, *)
-          json.each do |k, v|
-            self << Feature::Hypermedia::Hyperlink.new(:rel => k, :href => v)
+        
+        module ClassMethods
+          def links_definition_options
+            options = super
+            options[1] = {:class => Feature::Hypermedia::LinkCollection, :from => :_links, :extend => LinkCollectionRepresenter}
+            options
           end
-          self
-        end
-      end
-      
-      
-      module ClassMethods
-        def links_definition_options
-          options = super
-          options[1] = {:class => Feature::Hypermedia::LinkCollection, :from => :_links, :extend => LinkCollectionRepresenter}
-          options
         end
       end
     end
--- test/hal_json_test.rb
+++ test/hal_json_test.rb
@@ -2,25 +2,43 @@ require 'roar/representer/json/hal'
 
 class HalJsonTest < MiniTest::Spec
   module SongRepresenter
-    include Roar::Representer::JSON::HAL
+    include Roar::Representer::JSON
+    include Roar::Representer::JSON::HAL::Links
     
     link :self do
       "http://self"
     end
+    
+    link :next do
+      "http://hit"
+    end
   end
-      
-  describe "HAL/JSON" do
+  
+  describe "JSON::HAL::Links" do
     before do
       @song = Object.new.extend(SongRepresenter)
     end
     
-    it "what" do
-      assert_equal "{\"_links\":{\"self\":\"http://self\"}}", @song.to_json
+    it "render links" do
+      assert_equal "{\"_links\":{\"self\":\"http://self\",\"next\":\"http://hit\"}}", @song.to_json
     end
     
     it "parses incoming JSON links correctly" do
       @song.from_json "{\"_links\":{\"self\":\"http://self\"}}"
       assert_equal "http://self", @song.links[:self]
+      assert_equal nil, @song.links[:next]
     end
   end
+  
+  
+  describe "HAL/JSON" do
+    before do
+      @song = Object.new.extend(SongRepresenter)
+    end
+    
+    
+  end
+  
+  
+  
 end
