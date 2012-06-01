859ea7d40764d5f273fa704da69669a0f8ab8233
David Justice <david@devigned.com>
2012-06-01 16:13:31 -0400

    Serialize and deserialize any attribute for link elements.

--- lib/roar/representer/xml.rb
+++ lib/roar/representer/xml.rb
@@ -16,45 +16,51 @@ module Roar
           include InstanceMethods # otherwise Representable overrides our #to_xml.
         end
       end
-      
+
       module InstanceMethods
         def to_xml(*args)
           before_serialize(*args)
           super
         end
-        
+
         # Generic entry-point for rendering.
         def serialize(*args)
           to_xml(*args)
         end
-        
+
         def deserialize(*args)
           from_xml(*args)
         end
       end
-      
-      
+
+
       module ClassMethods
         include Representable::XML::ClassMethods
-        
+
         def links_definition_options
           [:links, {:from => :link, :class => Feature::Hypermedia::Hyperlink, :collection => true, :extend => XML::HyperlinkRepresenter}]
         end
-        
+
         # Generic entry-point for parsing.
         def deserialize(*args)
           from_xml(*args)
         end
       end
-      
+
       module HyperlinkRepresenter
         include XML
-        
+
         self.representation_wrap = :link
-        
+
+        def update_properties_from(doc, options, format)
+          self.marshal_load(doc)
+          self
+        end
+
         class << self
           alias_method :representable_extended, :extended
-          def extended( base )
+
+          def extended(base)
             representable_extended base
 
             base.marshal_dump.keys.each do |attributes|
--- test/json_representer_test.rb
+++ test/json_representer_test.rb
@@ -10,43 +10,43 @@ class JsonRepresenterTest < MiniTest::Spec
     property :pending
     attr_accessor :id, :pending
   end
-  
-  
+
+
   describe "JsonRepresenter" do
     before do
       @order = Order.new
     end
-    
-    
+
+
     describe "#to_json" do
       before do
         @order.id = 1
       end
-      
+
       it "returns the serialized model" do
         assert_equal '{"id":1}', @order.to_json
       end
-      
+
       it "is aliased by #serialize" do
         assert_equal '{"id":1}', @order.serialize
       end
-      
+
       it "accepts :include and :except" do
         assert_equal '{}', @order.to_json(:except => [:id])
       end
     end
-    
+
     describe "#from_json" do
       it "returns the deserialized model" do
         @order.from_json('{"id":1}')
         assert_equal 1, @order.id
       end
-      
+
       it "is aliased by #deserialize" do
         @order.deserialize('{"id":1}')
         assert_equal 1, @order.id
       end
-      
+
       it "works with a nil document" do
         assert @order.from_json(nil)
       end
@@ -54,13 +54,13 @@ class JsonRepresenterTest < MiniTest::Spec
       it "works with an empty document" do
         assert @order.from_json('')
       end
-      
+
       it "accepts :include and :except" do
         @order.from_json('{"id":1}', :except => [:id])
         assert_equal nil, @order.id
       end
     end
-    
+
     describe "JSON.from_json" do
       it "is aliased by #deserialize" do
         @order = Order.deserialize('{"id":1}')
@@ -76,19 +76,19 @@ class JsonHyperlinkRepresenterTest
     before do
       @link = Roar::Representer::Feature::Hypermedia::Hyperlink.new.extend(Roar::Representer::JSON::HyperlinkRepresenter).from_json({:rel => :self, :href => "http://roar.apotomo.de", :media => "web"}.to_json)
     end
-    
+
     it "responds to #rel" do
       assert_equal "self", @link.rel
     end
-    
+
     it "responds to #href" do
       assert_equal "http://roar.apotomo.de", @link.href
     end
-    
+
     it "responds to #media" do
       assert_equal "web", @link.media
     end
-    
+
     it "responds to #to_json" do
       assert_equal "{\"rel\":\"self\",\"href\":\"http://roar.apotomo.de\",\"media\":\"web\"}", @link.to_json
     end
@@ -103,41 +103,52 @@ class JsonHypermediaTest
         include Roar::Representer::JSON
         include Roar::Representer::Feature::Hypermedia
         attr_accessor :id, :self, :next
-        
+
         property :id
-        
+
         link :self do "http://self" end
         link :next do "http://next/#{id}" end
-      end
-      
+        end
+
       @r = @c.new
     end
-    
+
     it "responds to #links" do
       assert_equal [], @r.links
     end
-    
+
     it "extracts links from JSON" do
       @r = @c.from_json({:links => [{:rel => "self", :href => "http://self"}]}.to_json)
-      
+
       assert_equal 1, @r.links.size
-      assert_equal(["self", "http://self"], [@r.links.first.rel, @r.links.first.href]) 
+      assert_equal(%w(self http://self), [@r.links.first.rel, @r.links.first.href])
     end
-    
+
+    it "extracts links from JSON with any attributes" do
+      @r = @c.from_json({:links => [{:rel => "self", :href => "http://self", :template => "true", :blah => "123"}]}.to_json)
+
+      assert_equal 1, @r.links.size
+      assert_equal(%w(self http://self true 123), [@r.links.first.rel, @r.links.first.href, @r.links.first.template, @r.links.first.blah])
+    end
+
     it "renders link: correctly in JSON" do
       assert_equal "{\"id\":1,\"links\":[{\"rel\":\"self\",\"href\":\"http://self\"},{\"rel\":\"next\",\"href\":\"http://next/1\"}]}", @c.new(:id => 1).to_json
     end
-    
+
     it "doesn't render links when empty" do
       assert_equal("{\"links\":[]}", Class.new do
         include Roar::Representer::JSON
         include Roar::Representer::Feature::Hypermedia
-        
-        link :self do nil end
-        link :next do false end
+
+        link :self do
+          nil
+        end
+        link :next do
+          false
+        end
       end.new.to_json)
     end
-    
+
   end
 end
 
