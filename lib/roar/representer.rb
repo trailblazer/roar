ea8ce09905599cae2d6143b31aeea248649fabcf
Nick Sutterer <apotonick@gmail.com>
2013-02-28 14:30:46 +1100

    moving the ::inheritable_array method and friends to Representer.

--- lib/roar/representer.rb
+++ lib/roar/representer.rb
@@ -2,11 +2,38 @@ require 'representable'
 
 module Roar
   module Representer
+    # TODO: move to separate module
+    # DISCUSS: experimental. this will soon be moved to a separate gem
+    module InheritableArray
+      def representable_attrs
+        super.extend(ConfigExtensions)
+      end
+
+      module ConfigExtensions
+        def inheritable_array(name)
+          inheritable_arrays[name] ||= []
+        end
+        def inheritable_arrays
+          @inheritable_arrays ||= {}
+        end
+
+        def inherit(parent)
+          super
+          
+          parent.inheritable_arrays.keys.each do |k|
+            inheritable_array(k).push *parent.inheritable_array(k).clone
+          end
+        end
+      end
+    end
+
     def self.included(base)
       base.class_eval do
         include Representable
       end
     end
+
+    include InheritableArray
 
   private
     def before_serialize(*)
