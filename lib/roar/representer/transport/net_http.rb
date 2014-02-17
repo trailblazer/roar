08de9d2efc6fb474e6cac1e59c8131c5e6ce33e9
Nick Sutterer <apotonick@gmail.com>
2014-02-17 15:15:29 +1100

    deprecate the old #get_uri API properly.

--- lib/roar/representer/transport/net_http.rb
+++ lib/roar/representer/transport/net_http.rb
@@ -5,7 +5,10 @@ module Roar
   module Representer
     # Implements the (HTTP) transport interface with Net::HTTP.
     module Transport
-      # Definitions: every call returns a Response object responding to #body.
+      # Low-level interface for HTTP. The #get_uri and friends accept an options and an optional block, invoke
+      # the HTTP request and return the request object.
+      #
+      # The following options are available:
       class NetHTTP
         class Request # TODO: implement me.
           def initialize(options)
@@ -28,28 +31,29 @@ module Roar
           end
         end
 
-        def get_uri(options, &block)
-          call(Net::HTTP::Get, options, &block)
+        def get_uri(*options, &block)
+          call(Net::HTTP::Get, *options, &block)
         end
 
-        def post_uri(options, &block)
-          call(Net::HTTP::Post, options, &block)
+        def post_uri(*options, &block)
+          call(Net::HTTP::Post, *options, &block)
         end
 
-        def put_uri(options, &block)
-          call(Net::HTTP::Put, options, &block)
+        def put_uri(*options, &block)
+          call(Net::HTTP::Put, *options, &block)
         end
 
-        def delete_uri(options, &block)
-          call(Net::HTTP::Delete, options, &block)
+        def delete_uri(*options, &block)
+          call(Net::HTTP::Delete, *options, &block)
         end
 
-        def patch_uri(options, &block)
-          call(Net::HTTP::Patch, options, &block)
+        def patch_uri(*options, &block)
+          call(Net::HTTP::Patch, *options, &block)
         end
 
       private
-        def call(what, options, &block)
+        def call(what, *args, &block)
+          options = handle_deprecated_args(args)
           # TODO: generically handle return codes.
           do_request(what, options[:uri], options[:as], options[:body], options, &block)
         end
@@ -90,10 +94,23 @@ module Roar
         end
 
         def parse_uri(url)
-            uri = URI(url)
-            raise "Incorrect URL `#{url}`. Maybe you forgot http://?" if uri.instance_of?(URI::Generic)
-            uri
+          uri = URI(url)
+          raise "Incorrect URL `#{url}`. Maybe you forgot http://?" if uri.instance_of?(URI::Generic)
+          uri
+        end
+
+        def handle_deprecated_args(args) # TODO: remove in 1.0.
+          if args.size > 1
+            warn %{DEPRECATION WARNING: #get_uri, #post_uri, #put_uri, #delete_uri and #patch_uri no longer accept positional arguments. Please call them as follows:
+     get_uri(uri: "http://localhost/songs", as: "application/json")
+    post_uri(uri: "http://localhost/songs", as: "application/json", body: "{'id': 1}")
+Thank you and have a lovely day.}
+            return {:uri => args[0], :as => args[1]} if args.size == 2
+            return {:uri => args[0], :as => args[2], :body => args[1]}
           end
+
+          args.first
+        end
 
       end
 
