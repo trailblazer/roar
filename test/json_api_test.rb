ad234e1fb1708c6c63749a9e7e15ca4b5358390f
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2014-06-23 13:36:16 +0100

    Test for representation of a compound json-api document, including linked resources

--- test/json_api_test.rb
+++ test/json_api_test.rb
@@ -4,19 +4,27 @@ require 'roar/representer/json/json_api'
 class JsonApiTest < MiniTest::Spec
   let(:song) { OpenStruct.new(
                           title: 'Computadores Fazem Arte',
-                          album: OpenStruct.new(id: 1),
-                          musicians: [OpenStruct.new(id: 1), OpenStruct.new(id: 2)] 
+                          album: OpenStruct.new(id: 1, title: 'Da Lama ao Caos'),
+                          musicians: [OpenStruct.new(id: 1, name: 'Chico Science'), 
+                                      OpenStruct.new(id: 2, name: 'Jorge Du Peixe')] 
                         ) }
 
   representer!([Roar::Representer::JSON::JsonApi]) do
     property :id
     property :title
 
-    link :album
-    link :musicians
+    link "songs.album" do
+      {
+        type: "album",
+        href: "http://example.com/albums/{songs.album}"
+      }
+    end
+    # link "songs.musicians", type: :musicians do
+    #   "http://example.com/musicians/{songs.musicians}"
+    # end
   end
 
-  subject { [song].extend(rpr) }
+  subject { song.extend(rpr) }
 
   describe "#to_json" do
     it "renders document" do
@@ -42,6 +50,55 @@ class JsonApiTest < MiniTest::Spec
             }]
           }}
         )
+    end
+
+    context "including representations of linked resources" do
+      before do
+        rpr.class_eval do
+          include_linked
+        end
+      end
+
+      it "renders document" do
+        subject.to_json.must_equal(
+            %{{
+              "links": {
+                "songs.album": {
+                  "href": "http://example.com/albums/{songs.album}",
+                  "type": "albums"
+                },
+                "songs.musicians": {
+                  "href": "http://example.com/musicians/{songs.musicians}",
+                  "type": "musicians"
+                }
+              },
+              "songs": [{
+                "id": "1",
+                "title": "Computadores Fazem Arte",
+                "links": {
+                  "album": "9",
+                  "musicians": [ "1", "2" ]
+                }
+              }],
+              "linked": {
+                "albums": [{
+                  "id": "9",
+                  "href": "http://example.com/albums/9",
+                  "title": "Da Lama ao Caos"
+                }],
+                "musicians": [{
+                  "id": "1",
+                  "href": "http://example.com/musicians/1",
+                  "name": "Chico Science"
+                }, {
+                  "id": "2",
+                  "href": "http://example.com/musicians/2",
+                  "name": "Jorge Du Peixe"
+                }]
+              }
+            }}
+          )
+      end
     end
   end
 
