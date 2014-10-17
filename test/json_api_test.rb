33ee8b6efa236f6a531ef2e1df263756dbeaba47
Nick Sutterer <apotonick@gmail.com>
2014-10-17 11:17:29 +1100

    add tests for JsonApi with decorator.

--- test/json_api_test.rb
+++ test/json_api_test.rb
@@ -78,79 +78,158 @@ class JsonApiTest < MiniTest::Spec
     end
   end
 
-  describe "singular" do
-    subject { song.extend(Singular) }
+  class SingularDecorator < Roar::Decorator
+    include Roar::JSON::JsonApi
+    type :songs
 
-    # to_json
-    it "renders xxx" do
-      subject.to_hash.must_equal(
-        {
-          "songs" => {
-            "id" => "1",
-            "title" => "Computadores Fazem Arte",
-            "links" => {
-              "album" => "9",
-              "musicians" => [ "1", "2" ],
-              "composer"=>"10",
-              "listeners"=>["8"]
-            }
-          },
-          "links" => {
-            "songs.album"=> {
-              "href"=>"http://example.com/albums/{songs.album}", "type"=>"album"
-            }
-          },
-          "linked" => {
-            "album"=> [{"title"=>"Hackers"}],
-            "musicians"=> [
-              {"name"=>"Eddie Van Halen"},
-              {"name"=>"Greg Howe"}
-            ]
-          }
-        }
-      )
+    property :id
+    property :title
+
+    # local per-model "id" links
+    links do
+      property :album_id, :as => :album
+      collection :musician_ids, :as => :musicians
+    end
+    has_one :composer
+    has_many :listeners
+
+
+    # global document links.
+    link "songs.album" do
+      {
+        type: "album",
+        href: "http://example.com/albums/{songs.album}"
+      }
     end
 
-    # from_json
-    it "parses" do
-      song = OpenStruct.new.extend(Singular)
-      song.from_hash(
-        {
-          "songs" => {
-            "id" => "1",
-            "title" => "Computadores Fazem Arte",
+    compound do
+      property :album do
+        property :title
+      end
+
+      collection :musicians do
+        property :name
+      end
+    end
+  end
+
+  [Singular, SingularDecorator].each do |representer|
+    describe "singular with #{representer}" do
+      subject { song.extend(Singular) }
+
+      # to_json
+      it "renders xxx" do
+        subject.to_hash.must_equal(
+          {
+            "songs" => {
+              "id" => "1",
+              "title" => "Computadores Fazem Arte",
+              "links" => {
+                "album" => "9",
+                "musicians" => [ "1", "2" ],
+                "composer"=>"10",
+                "listeners"=>["8"]
+              }
+            },
             "links" => {
-              "album" => "9",
-              "musicians" => [ "1", "2" ],
-              "composer"=>"10",
-              "listeners"=>["8"]
-            }
-          },
-          "links" => {
-            "songs.album"=> {
-              "href"=>"http://example.com/albums/{songs.album}", "type"=>"album"
+              "songs.album"=> {
+                "href"=>"http://example.com/albums/{songs.album}", "type"=>"album"
+              }
+            },
+            "linked" => {
+              "album"=> [{"title"=>"Hackers"}],
+              "musicians"=> [
+                {"name"=>"Eddie Van Halen"},
+                {"name"=>"Greg Howe"}
+              ]
             }
           }
-        }
-      )
+        )
+      end
 
-      song.id.must_equal "1"
-      song.title.must_equal "Computadores Fazem Arte"
-      song.album_id.must_equal "9"
-      song.musician_ids.must_equal ["1", "2"]
-      song.composer_id.must_equal "10"
-      song.listener_ids.must_equal ["8"]
+      # from_json
+      it "parses" do
+        song = OpenStruct.new.extend(Singular)
+        song.from_hash(
+          {
+            "songs" => {
+              "id" => "1",
+              "title" => "Computadores Fazem Arte",
+              "links" => {
+                "album" => "9",
+                "musicians" => [ "1", "2" ],
+                "composer"=>"10",
+                "listeners"=>["8"]
+              }
+            },
+            "links" => {
+              "songs.album"=> {
+                "href"=>"http://example.com/albums/{songs.album}", "type"=>"album"
+              }
+            }
+          }
+        )
+
+        song.id.must_equal "1"
+        song.title.must_equal "Computadores Fazem Arte"
+        song.album_id.must_equal "9"
+        song.musician_ids.must_equal ["1", "2"]
+        song.composer_id.must_equal "10"
+        song.listener_ids.must_equal ["8"]
+      end
     end
   end
 
 
   # collection with links
-  describe "collection with links and compound" do
-    subject { [song, song].extend(Singular.for_collection) }
+  [Singular, SingularDecorator].each do |representer|
+    describe "collection with links and compound" do
+      subject { Singular.for_collection.prepare([song, song]) }
 
-    # to_json
+      # to_json
+      it do
+        subject.to_hash.must_equal(
+          {
+            "songs" => [
+              {
+                "id" => "1",
+                "title" => "Computadores Fazem Arte",
+                "links" => {
+                  "album" => "9",
+                  "musicians" => [ "1", "2" ],
+                  "composer"=>"10",
+                "listeners"=>["8"]
+                }
+              }, {
+                "id" => "1",
+                "title" => "Computadores Fazem Arte",
+                "links" => {
+                  "album" => "9",
+                  "musicians" => [ "1", "2" ],
+                  "composer"=>"10",
+                "listeners"=>["8"]
+                }
+              }
+            ],
+            "links" => {
+              "songs.album" => {
+                "href" => "http://example.com/albums/{songs.album}",
+                "type" => "album" # DISCUSS: does that have to be albums ?
+              },
+            },
+            "linked"=>{
+              "album"    =>[{"title"=>"Hackers"}], # only once!
+              "musicians"=>[{"name"=>"Eddie Van Halen"}, {"name"=>"Greg Howe"}]
+            }
+          }
+        )
+      end
+    end
+
+
+    # from_json
     it do
-      subject.to_hash.must_equal(
+      song1, song2 = Singular.for_collection.prepare([OpenStruct.new, OpenStruct.new]).from_hash(
         {
           "songs" => [
             {
@@ -160,82 +239,42 @@ class JsonApiTest < MiniTest::Spec
                 "album" => "9",
                 "musicians" => [ "1", "2" ],
                 "composer"=>"10",
-              "listeners"=>["8"]
-              }
-            }, {
-              "id" => "1",
-              "title" => "Computadores Fazem Arte",
+                "listeners"=>["8"]
+              },
+            },
+            {
+              "id" => "2",
+              "title" => "Talking To Remind Me",
               "links" => {
-                "album" => "9",
-                "musicians" => [ "1", "2" ],
-                "composer"=>"10",
-              "listeners"=>["8"]
+                "album" => "1",
+                "musicians" => [ "3", "4" ],
+                "composer"=>"2",
+                "listeners"=>["6"]
               }
-            }
+            },
           ],
           "links" => {
-            "songs.album" => {
-              "href" => "http://example.com/albums/{songs.album}",
-              "type" => "album" # DISCUSS: does that have to be albums ?
-            },
-          },
-          "linked"=>{
-            "album"    =>[{"title"=>"Hackers"}], # only once!
-            "musicians"=>[{"name"=>"Eddie Van Halen"}, {"name"=>"Greg Howe"}]
+            "songs.album"=> {
+              "href"=>"http://example.com/albums/{songs.album}", "type"=>"album"
+            }
           }
         }
       )
+
+      song1.id.must_equal "1"
+      song1.title.must_equal "Computadores Fazem Arte"
+      song1.album_id.must_equal "9"
+      song1.musician_ids.must_equal ["1", "2"]
+      song1.composer_id.must_equal "10"
+      song1.listener_ids.must_equal ["8"]
+
+      song2.id.must_equal "2"
+      song2.title.must_equal "Talking To Remind Me"
+      song2.album_id.must_equal "1"
+      song2.musician_ids.must_equal ["3", "4"]
+      song2.composer_id.must_equal "2"
+      song2.listener_ids.must_equal ["6"]
     end
-  end
-
-
-  # from_json
-  it do
-    song1, song2 = [OpenStruct.new, OpenStruct.new].extend(Singular.for_collection).from_hash(
-      {
-        "songs" => [
-          {
-            "id" => "1",
-            "title" => "Computadores Fazem Arte",
-            "links" => {
-              "album" => "9",
-              "musicians" => [ "1", "2" ],
-              "composer"=>"10",
-              "listeners"=>["8"]
-            },
-          },
-          {
-            "id" => "2",
-            "title" => "Talking To Remind Me",
-            "links" => {
-              "album" => "1",
-              "musicians" => [ "3", "4" ],
-              "composer"=>"2",
-              "listeners"=>["6"]
-            }
-          },
-        ],
-        "links" => {
-          "songs.album"=> {
-            "href"=>"http://example.com/albums/{songs.album}", "type"=>"album"
-          }
-        }
-      }
-    )
-
-    song1.id.must_equal "1"
-    song1.title.must_equal "Computadores Fazem Arte"
-    song1.album_id.must_equal "9"
-    song1.musician_ids.must_equal ["1", "2"]
-    song1.composer_id.must_equal "10"
-    song1.listener_ids.must_equal ["8"]
-
-    song2.id.must_equal "2"
-    song2.title.must_equal "Talking To Remind Me"
-    song2.album_id.must_equal "1"
-    song2.musician_ids.must_equal ["3", "4"]
-    song2.composer_id.must_equal "2"
-    song2.listener_ids.must_equal ["6"]
   end
 
 
