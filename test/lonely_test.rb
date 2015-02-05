b8fb52151f2e1ff31f77f07f7656eac887ac4f17
Hilary Holz <hilary@hholz.com>
2015-02-05 11:54:58 -0800

    hook Representable::Hash::Collection into Roar::JSON
    Thus gaining access to client methods, etc. Roar::JSON::Hash is
    equal to Representable::JSON::Hash, but Roar::JSON::Collection
    incorporates additional methods.

--- test/lonely_test.rb
+++ test/lonely_test.rb
@@ -4,6 +4,5 @@ require "roar/json/collection"
 require "roar/json/hash"
 
 class LonelyCollectionTest < MiniTest::Spec
-  it { Roar::JSON::Collection.must_equal Representable::JSON::Collection }
   it { Roar::JSON::Hash.must_equal Representable::JSON::Hash }
 end
