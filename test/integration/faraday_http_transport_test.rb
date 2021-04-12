5bea2bc9edf0015a43dd14c72965364b5687eff2
Andreas Schwarz <andreas.schwarz@hetzner.com>
2021-04-12 15:09:48 +0200

    fix raised error class

--- test/integration/faraday_http_transport_test.rb
+++ test/integration/faraday_http_transport_test.rb
@@ -61,7 +61,7 @@ class FaradayHttpTransportTest < MiniTest::Spec
 
     describe 'server errors (500 Internal Server Error)' do
       it '#get_uri raises a ClientError' do
-        assert_raises(Faraday::ClientError) do
+        assert_raises(Faraday::ServerError) do
           @transport.get_uri(uri: 'http://localhost:4567/deliberate-error', as: as).body
         end
       end
